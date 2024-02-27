0750d228aef30d306210a33a072cab281dbc6772
ndelucca <nazadelucca@gmail.com>
2024-02-27 19:30:21 -0300

    setup production for ndelucca.pythonanywhere.com

--- commander_api/commander_api/urls.py
+++ commander_api/commander_api/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("timekeeper/", include("apps.timekeeper.urls")),
+    re_path(
+        r"^favicon\.ico$",
+        RedirectView.as_view(url="/static/favicon.ico", permanent=True),
+    ),
 ]
