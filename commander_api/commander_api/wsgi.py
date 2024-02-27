61d3440a4f9472965037f1406f56423b2323b986
ndelucca <nazadelucca@gmail.com>
2024-02-27 18:48:02 -0300

    setup production for ndelucca.pythonanywhere.com

--- commander_api/commander_api/wsgi.py
+++ commander_api/commander_api/wsgi.py
@@ -7,9 +7,16 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
+# wsgi for pythonanywhere.com
+
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+
+path = "/home/ndelucca/dev/commander_api"
+if path not in sys.path:
+    sys.path.insert(0, path)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "commander_api.settings.production")
 
